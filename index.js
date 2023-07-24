26e31aaa44bf3286dd6b0946a30d5842c2d05215
amit142 <71812616+amit142@users.noreply.github.com>
2023-07-24 18:22:45 +0300

    first version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const form =document.getElementById('form')
 const nameInput =document.getElementById('name-input')
 const numberOfArriving =document.getElementById('arriving-number')
 const weekendStay = document.getElementById('weekend-stay')
+const ifChecked = document.getElementById('if-checked')
 const pics = ["./photos/1.JPG","./photos/2.JPG","./photos/3.JPG","./photos/4.JPG"]
 
 
@@ -24,4 +25,13 @@ form.addEventListener('submit',function (e){
     }
     console.log(guest)
     alert("לכו להזדיין")
+})
+weekendStay.addEventListener('change',function (){
+    if(weekendStay.checked){
+        console.log("hello")
+        ifChecked.innerText = `יהיה כיך יא בולבולים`
+    }
+    else{
+        ifChecked.innerText = ""
+    }
 })
